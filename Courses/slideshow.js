78e125d569296675f1e8f12cce24338a50bbd655
Jishnu P.S <jishnu82003@gmail.com>
2020-07-22 22:19:30 +0530

    Tried to make slideshow automatic, works somewhat

--- Courses/slideshow.js
+++ Courses/slideshow.js
@@ -1,14 +1,19 @@
+//slideshow with buttons
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
+  clearTimeout(timer);
+  setTimeout(showSlidesAutomatic, 5000);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
+  clearTimeout(timer);
+  setTimeout(showSlidesAutomatic, 5000);
 }
 
 function showSlides(n) {
@@ -27,17 +32,19 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-// var slideIndex = 0;
-// showSlides();
 
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
+//slideshow automatic
+ var slideIndex = 0;
+ showSlidesAutomatic();
+
+ function showSlidesAutomatic() {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {slideIndex = 1}
+   slides[slideIndex-1].style.display = "block";
+   setTimeout(showSlidesAutomatic, 5000); // Change image every 5 seconds
+ }
